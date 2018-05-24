eae7de7dbbb009766fdc403c7c046001de38a2bd
Andrew Xuan <andrewxuan2015@gmail.com>
2018-05-24 16:26:34 +0000

    first commit

---
+++ selection.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+  int b = 3;
+  // if statement to test is a is equal to 0
+  if (a == 0)
+  {
+    printf("a is 0\n");
+  }
+  else
+  {
+    printf("a is not 0\n");
+  }
+  if (a == b)
+{
+printf("a is equal to b\n");
+}
+else
+{
+printf("a is not equal to b\n");
+}
+
+  if (a != b)
+{
+ printf("a is not equal to b\n");
+}
+else
+{
+printf("a is equal to b\n");
+}
+  if (a > b)
+{
+printf("a is greater than to b\n");
+}
+else
+{
+printf("a is less than to b\n");
+}
+if (a >= b)
+{
+printf("a is greater than or equal to b\n");
+}
+else
+{
+printf("a is not greater than or equal to b\n");
+}
+if (a == 0 && b == 0)
+{
+printf("a is equal to 0\n AND b is equal to than to 0\n");
+}
+else
+{
+printf("a is not equal to 0\n AND b is equal to than to 0\n");
+}
+if (a == 0 || b == 0)
+{
+printf("a is equal to 0\n OR b is equal to than to 0\n");
+}
+else
+{
+printf("a is not equal to 0\n OR b is equal to than to 0\n");
+}
+ if (a == 0 && b == 0)
+{
+printf("a is equal to 0\n AND b is equal to than to 0\n");
+}
+else
+{
+printf("a is not equal to 0\n AND b is equal to than to 0\n");
+}
+if (a != 0)
+{
+printf("a is NOT equal to 0\n");
+}
+else
+{
+printf("a is equal to 0\n");
+}
+
+}
+
+
+
+
