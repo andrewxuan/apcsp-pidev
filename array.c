71d35f9401eeb3d1052f9ba687fd501256ec19b2
Andrew Xuan <andrewxuan2015@gmail.com>
2018-05-26 19:13:02 -0700

    Final Commit

--- array.c
+++ array.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
-void arrayAdd(int s, int n, int* arr)
+	void arrayAdd(int arr[],int s, int n)
 {
-   for (int i = 0; i < s; i++)
-  {
-       *(arr + i) = *(arr +i) +  n;
-  }
+	for (int i = 0; i < s; i++)
+{	arr[i] = arr[i] + n;
+	printf("%d\n", arr[i]);
+	}
 }
+
+
 int main()
 {
-  int arr[100];
-  for (int i = 0; i < 100; i++)
-  {
-    arr[i] = i * i;
-//    printf("%d ", arr[i]);
-  }
-  arrayAdd(100, 3, arr);
-  for (int i = 0; i < 100; i++)
-  {
-        printf("%d\n", arr[i]);
-  }
+	int arr[100];
+for (int i = 0; i < 100; i++)
+{
+	arr[i] = i * i;
 }
+arrayAdd(arr, 100, 2);
+} 
